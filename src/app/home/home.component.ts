910d69f82d19ea3b5c6d4fb039672c3967bb4c9b
abbas <abbasas690@gmail.com>
2024-05-01 09:27:19 +0530

    done till routing

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,21 @@
+import { Component,inject,Inject } from '@angular/core';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { HousingLocation } from '../housinglocation';
+import { CommonModule } from '@angular/common';
+import { HousingService } from '../housing.service';
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [HousingLocationComponent,CommonModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
+  providers:[HousingService]
+})
+export class HomeComponent {
+  housingLocationList: HousingLocation [] ;
+  housingService: HousingService = inject(HousingService)
+                                         
+  constructor(){
+    this.housingLocationList = this.housingService.getAllHousingLocations()
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+import { bootstrapApplication, provideProtractorTestingSupport } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-
-bootstrapApplication(AppComponent, appConfig)
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
+bootstrapApplication(AppComponent, {
+  providers:[provideProtractorTestingSupport(),
+    provideRouter(routes)]
+})
   .catch((err) => console.error(err));
